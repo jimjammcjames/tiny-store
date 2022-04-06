14ff2427be6d91ea1f8ffebb2746293269401720
jamesdugle <jamesdugle@microsoft.com>
2022-04-05 17:39:17 -0700

    Initial implementation

--- server.cs
+++ server.cs
@@ -1,12 +1,70 @@
 // Hello World! program
 using System;
- 
-namespace HelloWorld
-{
-    class Hello {         
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+using System.Collections.Generic;
+
+class Server {   
+
+    static void Main(string[] args)
+    {
+        DataBase.User james = new DataBase.User("123");
+
+        DataBase.Item banana = new DataBase.Item("123", "banana", 2.25);
+        james.addToCart(banana);
     }
+}
+
+namespace DataBase
+{
+    class User
+    {
+        public string ID;
+        ShoppingCart cart;
+        
+        public User(string ID)
+        {
+            this.ID = ID;  
+            this.cart = new ShoppingCart();  
+            Console.WriteLine("Created "+ ID);
+        }
+
+        public void addToCart(Item item)
+        {
+            this.cart.addItem(item);
+            Console.WriteLine("added " + item.name);
+        }
+
+        
+
+    }
+
+    class ShoppingCart
+        {
+           List<Item> item_list;
+
+           public ShoppingCart()
+           {
+               item_list = new List<Item>();
+           }
+
+           public void addItem(Item item)
+           {
+               this.item_list.Add(item);
+           }
+        }
+
+    class Item
+    {
+        public string ID;
+        public string name;
+        public double price;
+
+        public Item(string ID, string name, double price)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.price = price;
+        }
+        
+    }
+
 }
