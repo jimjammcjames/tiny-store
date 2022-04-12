69c29572204eedae81a4a1d34926003afd83c55d
jamesdugle <jamesdugle@microsoft.com>
2022-04-12 10:18:21 -0700

    restructured architecture

--- server.cs
+++ server.cs
@@ -2,69 +2,123 @@
 using System;
 using System.Collections.Generic;
 
-class Server {   
-
-    static void Main(string[] args)
+static class Server {   
+    static DataBase dataBase = new DataBase();
+    private static Random random = new Random();
+    public static void Main()
     {
-        DataBase.User james = new DataBase.User("123");
-
-        DataBase.Item banana = new DataBase.Item("123", "banana", 2.25);
+        User james = createUser("james");
+        Item banana = createItem("banana", 2.50);
         james.addToCart(banana);
+        // james.getCart();
+        dataBase.printInfo();
+    }
+    static void populate()
+    {
+        
+    }
+    static string generateKey()
+    {
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var stringChars = new char[8];
+
+        for (int i = 0; i < stringChars.Length; i++)
+        {
+            stringChars[i] = chars[random.Next(chars.Length)];
+        }
+
+        return new String(stringChars);
+    }
+    static User createUser(string name)
+    {
+        string id = generateKey();
+        User newUser = new User(id, name);
+        dataBase.accounts.Add(id, newUser);
+        return newUser;
+    }
+    static Item createItem(string name, double price)
+    {
+        string id = generateKey();
+        Item newItem = new Item(id, name, price);
+        dataBase.catalogue.Add(id, newItem);
+        return newItem;
     }
 }
 
-namespace DataBase
+class DataBase
 {
-    class User
+    public Dictionary<string, Item> catalogue;
+    public Dictionary<string, User> accounts;
+    public DataBase()
     {
-    class ShoppingCart
-    {
-        List<Item> item_list;
-
-        public ShoppingCart()
-        {
-            item_list = new List<Item>();
-        }
-        public void addItem(Item item)
-        {
-            this.item_list.Add(item);
-        }
-    }   
-        public string ID;
-        ShoppingCart cart;
-        
-        public User(string ID)
-        {
-            this.ID = ID;  
-            this.cart = new ShoppingCart();  
-            Console.WriteLine("Created "+ ID);
-        }
-
-        public void addToCart(Item item)
-        {
-            this.cart.addItem(item);
-            Console.WriteLine("added " + item.name);
-        }
-
-        
-
+        catalogue = new Dictionary<string, Item>();
+        accounts = new Dictionary<string, User>();
     }
 
-    
-
-    class Item
-    {
-        public string ID;
-        public string name;
-        public double price;
-
-        public Item(string ID, string name, double price)
+    public void printInfo(){
+        Console.WriteLine("---accounts in dataBase---");
+        foreach (var account in this.accounts)
         {
-            this.ID = ID;
-            this.name = name;
-            this.price = price;
+            Console.WriteLine(account.Key + " " + account.Value.name);
         }
-        
+        Console.WriteLine("---catalogue---");
+        foreach (var item in this.catalogue)
+        {
+            Console.WriteLine(item.Key + " " + item.Value.name);
+        }
+    }
+}
+class User
+{
+    public string ID;
+    public string name;
+    List<Item> cart;
+    
+    public User(string ID, string name)
+    {
+        this.name = name;
+        this.ID = ID;  
+        this.cart = new List<Item>();  
+        Console.WriteLine("Created "+ name);
+    }
+
+    public void addToCart(Item item)
+    {
+        this.cart.Add(item);
+        Console.WriteLine("added " + item.name);
+    }
+
+    public void removeFromCart(Item item)
+    {
+        this.cart.Remove(item);
+        Console.WriteLine("removed" + item.name);
+    }
+
+    public List<Item> getCart()
+    {
+        Console.WriteLine("-----items in cart: -----");
+        foreach (var item in this.cart)
+        {
+           Console.WriteLine(item.name); 
+        }
+        Console.WriteLine("---------------");
+        return this.cart;
     }
 
 }
+
+class Item
+{
+    public string ID;
+    public string name;
+    public double price;
+
+    public Item(string ID, string name, double price)
+    {
+        this.ID = ID;
+        this.name = name;
+        this.price = price;
+    }
+    
+}
+
