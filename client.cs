80d35b7c93e30087833aeda78fc4572a8b95fdac
jamesdugle <jamesdugle@microsoft.com>
2022-05-25 14:31:21 -0700

    added messaging between server and client

--- client.cs
+++ client.cs
@@ -6,7 +6,26 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Starting client");
+            Console.WriteLine("Enter your username:");
+            var username = Console.ReadLine();
+            
+            var socket = new System.Net.Sockets.TcpClient(System.Net.IPAddress.Loopback.ToString(), 8080);
+            var stream = socket.GetStream();
+            while (true){
+                Console.WriteLine("Enter message");
+                var message = Console.ReadLine();
+                var writer = new System.IO.StreamWriter(stream);
+                writer.WriteLine(message);
+                writer.Flush();
+                  
+                System.Byte[] bytes = new System.Byte[2048];
+                int size = stream.Read(bytes, 0, bytes.Length);
+                var askii = new System.Text.ASCIIEncoding();
+                string response = askii.GetString(bytes, 0, size-2);
+                Console.WriteLine(response);
+            }
         }
     }
 }
--- server.cs
+++ server.cs
@@ -1,21 +1,59 @@
-// Hello World! program
+// deploy on kubernetes
 using System;
 using System.Collections.Generic;
 
 static class Server {   
-    static DataBase dataBase = new DataBase();
+    static Database database = new Database();
     private static Random random = new Random();
     public static void Main()
     {
-        User james = createUser("james");
-        Item banana = createItem("banana", 2.50);
-        james.addToCart(banana);
-        // james.getCart();
-        dataBase.printInfo();
+        populate();
+        // createUser("james");
+        // database.printInfo();
+        // List<KeyValuePair<string, Item>> results = database.searchCatalogue("shoe");
+        
+        var endpoint = new System.Net.IPEndPoint(System.Net.IPAddress.Any ,8080);
+        var listener = new System.Net.Sockets.TcpListener(endpoint);
+        listener.Start();
+        Console.WriteLine("listening on stream");
+        var client = listener.AcceptTcpClient();
+        var stream = client.GetStream();
+        listenOnStream(stream);
+    }
+    static void listenOnStream(System.Net.Sockets.NetworkStream stream){
+        while (true){
+            System.Byte[] bytes = new System.Byte[2048];
+            int size = stream.Read(bytes, 0, bytes.Length);
+            var askii = new System.Text.ASCIIEncoding();
+            string message = askii.GetString(bytes, 0, size-2);
+            Console.WriteLine(message);
+            parseCommand(message);
+
+            var writer = new System.IO.StreamWriter(stream);
+            writer.WriteLine("successful");
+            writer.Flush();
+                  
+        }
+    }
+    static void parseCommand(string command){
+        if (command == "sysInfo"){
+            database.printInfo();
+        }
+        if ( command.Split(' ')[0] == "search" ){
+            database.searchCatalogue(command.Split(' ')[1]);
+        }
     }
     static void populate()
     {
-        
+        Console.WriteLine("populating database");
+        createItem("banana", 2.50);
+        createItem("running shoes", 32.50);
+        createItem("basketball shoes", 32.50);
+        createItem("painting", 50.00);
+        createItem("basketball", 25.00);
+        createItem("blender", 20.50);
+        createItem("glasses", 50.50);
+        createItem("keyboard", 72.00);
     }
     static string generateKey()
     {
@@ -33,30 +71,30 @@ static class Server {
     {
         string id = generateKey();
         User newUser = new User(id, name);
-        dataBase.accounts.Add(id, newUser);
+        database.accounts.Add(id, newUser);
         return newUser;
     }
     static Item createItem(string name, double price)
     {
         string id = generateKey();
         Item newItem = new Item(id, name, price);
-        dataBase.catalogue.Add(id, newItem);
+        database.catalogue.Add(id, newItem);
         return newItem;
     }
 }
 
-class DataBase
+class Database
 {
     public Dictionary<string, Item> catalogue;
     public Dictionary<string, User> accounts;
-    public DataBase()
+    public Database()
     {
         catalogue = new Dictionary<string, Item>();
         accounts = new Dictionary<string, User>();
     }
 
     public void printInfo(){
-        Console.WriteLine("---accounts in dataBase---");
+        Console.WriteLine("---accounts in database---");
         foreach (var account in this.accounts)
         {
             Console.WriteLine(account.Key + " " + account.Value.name);
@@ -66,6 +104,27 @@ class DataBase
         {
             Console.WriteLine(item.Key + " " + item.Value.name);
         }
+    }
+
+    public List<KeyValuePair<string, Item>> searchCatalogue(string input)
+    {
+        List<KeyValuePair<string, Item>> results = new List<KeyValuePair<string, Item>>();
+        foreach (KeyValuePair<string, Item> kvp in this.catalogue)
+        {
+            if (kvp.Value.name.Contains(input)){
+                results.Add(kvp);
+            }
+        }
+
+        Console.WriteLine("------ results for {0}: -----", input);
+        foreach (var kvp in results)
+        {
+            string key = kvp.Key;
+            Item result = kvp.Value;
+            Console.WriteLine(result.name);
+        }
+
+        return results;
     }
 }
 class User
